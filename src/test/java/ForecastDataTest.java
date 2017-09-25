34c0c0095a02326ab777ce7ff91dc3c289ee796a
Maria <maria.toomsalu@gmail.com>
2017-09-25 12:04:17 +0300

    1st assignment

---
+++ src/test/java/ForecastDataTest.java
@@ -0,0 +1,124 @@
+import org.junit.Test;
+
+public class ForecastDataTest {
+
+    @Test
+    public void testGetCityCoordinatesShouldReturnCityCoordinates(){
+
+    }
+
+    @Test
+    public void testGetCityNameShouldReturnCityName(){
+
+    }
+
+    @Test
+    public void testGetCityByIDShouldReturnCityByID(){
+
+    }
+
+    @Test
+    public void testGetCoordinatesShouldReturnCoordinatesInGEO(){
+
+    }
+
+    @Test
+    public void invalidLongitudeUnitShouldThrowIllegalArgumentExceptionShouldBeInDegree(){
+
+    }
+
+    @Test
+    public void invalidLatitudeUnitShouldThrowIllegalArgumentExceptionShouldBeInDegree(){
+
+    }
+
+    @Test
+    public void testGetCurrentWeatherByCoordinatesShouldReturnCurrentWeather(){
+
+    }
+
+    @Test
+    public void testGetCurrentWeatherWithDateAndTimeShouldReturnCurrentWeatherByDateAndTime(){
+
+    }
+
+    @Test
+    public void testGetThreeDayForecastByCityShouldReturnThreeDayForecast(){
+
+    }
+
+    @Test
+    public void testGetThreeObjectsContainingTwoTemperaturesEachShouldReturnThreeDayForecast(){
+
+    }
+
+    @Test
+    public void testGetThreeObjectsWithDatesShouldReturnThreeDayForecast(){
+
+    }
+
+    @Test
+    public void testGetWeatherDataShouldReturnAllDetails(){
+
+    }
+
+    @Test
+    public void testGetWeatherMapShouldReturnWeatherMap(){
+        
+    }
+
+    @Test
+    public void testGetYesterdaysWeatherShouldThrowExceptionBecauseItShouldShowOnlyTodayOrThreeDaysForecast(){
+
+    }
+
+    @Test
+    public void testGetWeatherAfterThreeDaysShouldThrowExceptionBecauseThreeFollowingDaysIsMax(){
+
+    }
+
+    @Test
+    public void testGetMinTemperatureWhenThreeDayForecastShouldReturnMinTemp(){
+
+    }
+
+    @Test
+    public void testGetMaxTemperatureWhenThreeDayForecastShouldReturnMaxTemp(){
+
+    }
+
+    @Test
+    public void testGetTemperatureShouldReturnFloat(){
+
+    }
+
+
+    @Test
+    public void testGetTemperatureUnitShouldReturnCelsius(){
+
+    }
+
+    @Test
+    public void testGetTemperatureUnitShouldReturnFahrenheit(){
+
+    }
+
+    @Test
+    public void testGetCurrentTemperatureShouldReturnCurrentTemp(){
+    }
+
+    @Test
+    public void testExceedsHighestTemperatureShouldThrowExceptionBecauseTemperatureIsHigherThanHighestAllowed(){
+
+    }
+
+    @Test
+    public void testMinTemperatureBiggerThanMaxShouldThrowExceptionBecauseMinHasToBeSmallerOrEqual(){
+
+    }
+
+    @Test
+    public void testMaxTemperatureSmallerThanMinShouldThrowExceptionBecauseMaxHasToBeBiggerOrEqual(){
+
+    }
+}
---
+++ src/test/java/HttpUtilityTest.java
@@ -0,0 +1,25 @@
+/*
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class HttpUtilityTest {
+
+    @Test
+    public void testHttpSuccessfulConnectionToExampleApi(){
+        try{
+            String requestUrl = "<API REQUEST URL>";
+            HttpURLConnection con = HttpUtility.makeHttpGetRequest(requestUrl);
+
+            assertEquals(con.getResponseCode(), actual: 200);
+        }catch (IOException ioq){
+            fail("HTTP connection threw error:" + ioq.getLocalizedMessage());
+        }
+    }
+}
+*/
